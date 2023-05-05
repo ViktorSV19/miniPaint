ae6a4ef6ca560730ae275905c9183600d6bf1208
Viktor <vsamoylov819@gmail.com>
2023-05-05 16:24:29 +0300

    add save, and bind

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ const canvasHeight = canvas.clientHeight;
 
 let figure = [];
 
-canvas.addEventListener(`mousemove`, (e) => {
+let canvasDownload = document.getElementById(`canvasDownload`);
+let pngDataURL;
+
+canvas.onmousemove = function (e) {
     const x = e.offsetX;
     const y = e.offsetY;
     let startPos;
@@ -31,12 +34,67 @@ canvas.addEventListener(`mousemove`, (e) => {
     canvas.onclick = function () {
         figure.push([x, y]);
         if (figure.length >= 2) {
-            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            // ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            ctx.beginPath();
+            ctx.fillStyle = `#fff491`;
+            ctx.rect(0, 0, canvasWidth, canvasHeight);
+            ctx.fill();
             drawFigure(figure);
             startPos = [figure[lastIndex(figure)][0], figure[lastIndex(figure)][1]];
         }
+        pngDataURL = canvas.toDataURL(`image/png`);
+        canvasDownload.href = pngDataURL;
     };
+
+    canvas.oncontextmenu = function () {
+        canvas.onmousemove = null;
+        return false;
+    };
+};
+
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'KeyR') {
+        figure.length = 0;
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    }
 });
+
+// canvas.addEventListener(`mousemove`, (e) => {
+//     const x = e.offsetX;
+//     const y = e.offsetY;
+//     let startPos;
+
+//     if (figure.length >= 2) {
+//         startPos = [figure[lastIndex(figure)][0], figure[lastIndex(figure)][1]];
+//         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+//         drawFigure(figure);
+//         ctx.beginPath();
+//         ctx.moveTo(startPos[0], startPos[1]);
+//         ctx.lineTo(x, y);
+//         ctx.stroke();
+//     } else if (figure.length === 1) {
+//         startPos = [figure[lastIndex(figure)][0], figure[lastIndex(figure)][1]];
+//         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+//         ctx.beginPath();
+//         ctx.moveTo(startPos[0], startPos[1]);
+//         ctx.lineTo(x, y);
+//         ctx.stroke();
+//     }
+
+//     canvas.onclick = function () {
+//         figure.push([x, y]);
+//         if (figure.length >= 2) {
+//             ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+//             drawFigure(figure);
+//             startPos = [figure[lastIndex(figure)][0], figure[lastIndex(figure)][1]];
+//         }
+//     };
+
+//     canvas.oncontextmenu = function () {
+//         canvas.onmousemove = null;
+//         return false;
+//     };
+// });
 
 function lastIndex(array) {
     return array.length - 1;
